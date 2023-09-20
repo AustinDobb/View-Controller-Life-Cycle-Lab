d0f8cb9e9349432436332339c4679d97e0ab0667
Austin Dobberfuhl <AUSTIN.DOBBERFUHL4597@STU.MTEC.EDU>
2023-09-20 10:08:50 -0600

    Initial Commit

---
+++ OrderOfEvents/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderOfEvents
+//
+//  Created by Austin Dobberfuhl on 9/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
